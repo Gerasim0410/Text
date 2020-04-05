c5c7d2779913f8a246210e6dc46beb93877ff462
Тюрина Дарья <tyurina0410@gmail.com>
2020-04-05 20:09:00 +0300

    Update Source.cpp

--- Project34/Source.cpp
+++ Project34/Source.cpp
@@ -6,6 +6,15 @@ int main() {
 	TText t;
 	t.Read("123.txt");
 	t.PrintText();
+	int k = 1, str = 10;
+	for (t.Reset(); !t.IsEnd(); t.GoNext()) {
+		if (k == str) {
+			cout << t.GetLine() << endl;
+			break;
+		}
+		k++;
+	}
+
 	_getch();
 	return 0;
 }
